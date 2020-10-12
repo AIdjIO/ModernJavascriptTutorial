ef4f2db50b55a83c72e78e58b01a222fb849b96d
AIdjIO <ai.dj@pm.me>
2020-10-12 20:47:36 +0100

    added recursive function and micellanous

---
+++ JavascriptMicellanous/ternaryOperator.js
@@ -0,0 +1,12 @@
+let a = 0;
+let result;
+
+// if (a<1){
+//     result = 'less than 1'
+// } else {
+//     result = 'greater than or eqaul to 1'
+// }
+
+result = a < 1 ? 'less than 1' : 'greater than or equal to 1'
+
+document.getElementsByTagName('body')[0].innerHTML = result
---
+++ JavascriptRecursiveFunctions/recursiveFunctions.js
@@ -0,0 +1,71 @@
+function countDown(n){
+    for (let i=n; i>0; i--){
+        console.log(i)
+    }
+    console.log("count down ends")
+}
+
+function countDownRecursive(n){
+    if (n <=0){
+        console.log("count down ends")
+        return
+    } else {
+        console.log(n)
+        countDownRecursive(n-1)
+    }
+}
+
+function sumRange(n){
+    let total = 0;
+    for (let i=n; i>0; i--){
+        total+=i;
+    }
+    return total;
+}
+
+
+function sumRangeRecursive(n, total = 0){
+
+    if (n<=0){
+        return total
+    }
+    return sumRangeRecursive(n-1, n + total)
+}
+
+
+const tree = {
+    name:'John',
+    children:[
+        {
+            name:'Jim',
+            children:[]
+        },
+        {
+            name:'Zoe',
+            children: [
+                {name:'Kyle', children:[]},
+                {name:'Reece', children:[]}
+            ]
+        }
+    ]
+}
+
+function printChildrenRecursive(t){
+    if (t.children.length === 0 ){
+        return
+    }
+    t.children.forEach(child=>{
+        console.log(child.name);
+        printChildrenRecursive(child);
+    })
+}
+
+function printChildren(t){
+    if (!t.children){
+        return
+    }
+    t.children.forEach(child=>{
+        console.log(child.name);
+        printChildren(child)
+    })
+}
